ae225975501029ba84f58680cf64efb06b6ef15a
mean-stack-devhub-vaibhavi <vaibhaviyenare30@gmail.com>
2026-09-21 21:16:14 +0530

    feat: add new AI feature

--- PYTHON/Python Fundamentals Assignment1/assignment.py
+++ PYTHON/Python Fundamentals Assignment1/assignment.py
@@ -19,3 +19,71 @@
 
 
 #  Ask the user to enter two integers and one float. Convert them all to floats and print their average
+# num1 = int(input("Enter first number : "))
+# num2 = int(input("Enter second number : "))
+# num3 = float(input("Enter third number : "))
+
+# num1 = float(num1)
+# num2 = float(num2)
+
+# avg = (num1 + num2 + num3) / 3
+# print("Avg : ", avg )
+
+
+
+#  The user enters a string containing a number (e.g., ). Convert it to: "45"
+# • an integer
+# • a float
+# • a string again
+# Print all three values with their types
+
+# num = int(input("Enter a number : "))
+
+# string_num = str(num)
+# float_num = float(num)
+# int_num = int(num)
+
+# print("String : ", string_num)
+# print("Float : ", float_num)
+# print("Int : ", int_num)
+
+
+
+
+# . Evaluate and print the result of the following expression:
+# x = 10 + 3 * 2 ** 2
+# Based on what you learnt in the lecture explain why the output is what it is.
+
+# x = 10 + 3 * 2 ** 2
+# print(x)
+
+
+# Write a program to swap values of two numbers entered by the user.
+
+# num1 = int(input("Enter a first number : "))
+# num2 = int(input("Enter a second number : "))
+
+# print("Before swaping")
+# print("Num1 : ", num1)
+# print("Num2 : ",num2)
+
+# num1 , num2 = num2 , num1
+# print("Num1 : ", num1)
+# print("Num2 : ", num2)
+
+# Ask the user for a temperature in Celsius (string input). Convert it to ,
+# then calculate and print temperature in Fahrenheit. float
+# Conversion formula: F ahrenheitT emp = (CelsiusT emp ∗ (9/5)) + 32
+
+# Celsius = input("Enter a temperature in celsius : ")
+# Celsius = float(Celsius)
+# Fahrenheit = (Celsius *  (9/5)) + 32
+# print("Fahrenheit : ", Fahrenheit)
+
+
+# Take the radius ( ) as user input and print the area.Q8 r
+# Use the formula: π * (value of π = 3.14)
+
+r = float(input("Enter radius : "))
+area = 3.14 * (r ** 2)
+print("Area : ", area)
---
+++ PYTHON/conditional-statment.py
@@ -0,0 +1,57 @@
+# age = int(input("Enter age : "))
+# if age >= 18:
+#      print("You can vote")
+# else: 
+#      print("You can't vote")
+
+
+
+# num = int(input("Enter a number : "))
+# if num > 1: 
+#      print(num , " is positive")
+# else:
+#      print(num, " is nagative")
+
+
+# num = int(input("Enter a number "))
+# if num % 2 == 0:
+#      print(num , " is even")
+# else: 
+#      print(num , " is odd")
+
+
+# marks = int(input("Enter a marks : "))
+# if marks >= 80:
+#      print("very good")
+# elif marks >= 60:
+#      print("good")
+# elif marks >= 35:
+#      print("pass")
+# else:
+#      print("Try another time")
+
+
+# password = str(input("Enter password : "))
+# if password == "python123":
+#      print( "login successful")
+# else:
+#      print("try one more time")
+
+
+# num1 = int(input("Enter a number : "))
+# num2 = int(input("Enter a number : "))
+# if num1 >= num2:
+#      print(num1 , " is greter than number ", num2)
+# else:
+#      print(num2 , " is greter than number ", num1)
+
+
+username = input("Enter a username : ")
+password = input("Enter a password : ")
+if (username == "user123" and password ==  "pass123"):
+     print("Login successfuly")
+else:
+     if(username != "user123"):
+          print("username is wrong")
+     else:
+          print("password is wrong")
