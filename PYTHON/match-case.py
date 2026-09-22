374c9cee3ee1764d115a0893184577147d6cd159
mean-stack-devhub-vaibhavi <vaibhaviyenare30@gmail.com>
2026-09-22 15:08:28 +0530

    feat: add new AI feature

---
+++ PYTHON/match-case.py
@@ -0,0 +1,69 @@
+# days = input("Enter any one number in 1 to 7 number : ")
+# match days:
+#      case "1":
+#           print("Sunday")
+#      case "2":
+#           print("Monday")
+#      case "3":
+#           print("Tuesday")
+#      case "4":
+#            print("Wednessday")
+#      case "5":
+#           print("Thusday")
+#      case "6":
+#           print("Friday")
+#      case "7":
+#           print("Saturday")
+#      case _:
+#           print("You enter a wrong number!")
+                  
+
+# num1 = int(input("Enter number : "))
+# num2 = int(input("Enter number : "))
+# operater = input("Enter operater (+, -, *, /) : ")
+
+# match operater:
+#      case "+":
+#           print(num1 + num2)
+#      case "+":
+#           print(num1 - num2)
+#      case "+":
+#           print(num1 * num2)
+#      case "+":
+#           if num2 != 0:
+#                print(num1 / num2)
+#           else:
+#                print("Cannot divide by zero")
+#      case _:
+#           print("Invalid operator")
+
+
+
+# menu = input("1.Pizza 2.Burger 3.Sandwich 4.Exit")
+# num = input("Enter any number : ")
+
+# match num:
+#      case "1":
+#           print("Pizza")
+#      case "2":
+#           print("Burger")
+#      case "3":
+#           print("Sandwich")
+#      case "4":
+#           print("Exit")
+
+vowel = input(" Enter any one charecter a to z : ")
+
+match vowel:
+     case "a":
+          print(" a is vowel")
+     case "e":
+          print(" e is vowel")
+     case "i":
+          print(" i is vowel")
+     case "o":
+          print(" o is vowel")
+     case "u":
+          print(" u is vowel")
+     case _:
+          print(vowel ," is not vowel")
---
+++ PYTHON/while_loop.py
@@ -0,0 +1,31 @@
+# count = 1
+# while (count <= 5):
+#      print("Hello World")
+#      count += 1
+
+# number = 1
+# while (number <= 10):
+#   print(number)
+#   number += 1
+
+
+# number = 10
+# while (number >= 1):
+#      print(number)
+#      number -= 1
+
+# number = 2
+# while (number <= 20):
+#      print(number)
+#      number += 2
+
+# number = 3
+# while (number <= 20):
+#      print(number)
+#      number += 3
+
+
+num = int(input("Enter any number : "))
+while (num <= 10):
+  print(num)
+  num *= 10
