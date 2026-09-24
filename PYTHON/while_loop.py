dfc7e2156671d4ac9b1f1e553172162160afb39b
mean-stack-devhub-vaibhavi <vaibhaviyenare30@gmail.com>
2026-09-24 21:06:49 +0530

    feat: add new AI feature

--- PYTHON/while_loop.py
+++ PYTHON/while_loop.py
@@ -25,7 +25,42 @@
 #      number += 3
 
 
-num = int(input("Enter any number : "))
-while (num <= 10):
-  print(num)
-  num *= 10
+# num = int(input("Enter any number : "))
+# while (num <= 10):
+#   print(num)
+#   num *= 10
+
+# num = int(input("Enter a number : "))
+# i = 1
+# while i <= num:
+#      print(i)
+#      i += 1
+
+
+# num = int(input("Enter a number : "))
+# i = 1
+# while num >= 1:
+#      print(num)
+#      num -= 1
+
+# num = int(input("Enter a number : "))
+# i = 1
+# while i <= 10:
+#      print(num * i)
+#      i += 1
+
+
+# num = 1
+# while num <= 10:
+#   if num == 5:
+#       break
+#   print(num)
+#   num = num + 1
+
+i = 1
+while i <= 10:
+    if i % 3 == 0:
+      i += 1
+      continue
+    print(i)
+    i += 1
